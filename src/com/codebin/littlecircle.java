595929b0e69658ad2a20c578ec6a830ef06b7803
Hibou Violent <tommydhn@gmail.com>
2020-06-12 16:06:23 +0200

    wip

--- src/com/codebin/littlecircle.java
+++ src/com/codebin/littlecircle.java
@@ -15,7 +15,7 @@ public class littlecircle extends JComponent {
 
     public void paintComponent(Graphics g){
         //Vous verrez cette phrase chaque fois que la méthode sera invoquée
-        System.out.println("Je suis un petit cercle !");
+        //System.out.println("Je suis un petit cercle !");
         g.fillOval(5, 5, 15, 15);
     }
 
